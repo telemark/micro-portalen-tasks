9bebaab16215be590e5b2a53a36c80effa1587bc
Jonas Enge <jonas.enge@gmail.com>
2017-10-05 14:03:54 +0200

    Refactor

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
-  ENCRYPTOR_SECRET: process.env.ENCRYPTOR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   compilo: {
     url: process.env.COMPILO_URL || 'https://feed.klos.no',
     username: process.env.COMPILO_X1 || 'x1',
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { router, get } = require('microrouter')
-const readme = require('./lib/readme')
+const getReadme = require('./lib/get-readme')
 const getTasks = require('./lib/get-tasks')
 
 module.exports = router(
-  get('/', readme),
+  get('/', getReadme),
   get('/user/:user', getTasks)
 )
